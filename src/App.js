13c35ad323c980c0a361eed3576b9f628882d7d0
KHS <burdock2nd@duksung.ac.kr>
2023-05-15 21:02:37 +0900

    5/15 수업완료

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
-import './App.css';
 import Todo from './Todo'
 import AddTodo from "./AddTodo.js";
-import {Paper, List, Container} from "@material-ui/core";
-import { call } from "./service/ApiService";
+import './App.css';
+import {
+  Paper,
+  List,
+  Container,
+  Grid,
+  Button,
+  AppBar,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import { call, signout } from "./service/ApiService";
 
 class App extends React.Component {
   constructor(props) {
@@ -12,12 +21,15 @@ class App extends React.Component {
       items: [
         
       ],
+      /* (1) 로딩 중이라는 상태를 표현할 변수 생성자에 상태 변수를 추가한다.*/
+      loading: true,
     };
   }
 
   componentDidMount() {
+    /* (2) componentDidMount에서 Todo 리스트를 가져오는 GET 요청이 성공적으로 리턴하는 경우 loading을 false로 고친다. 더 이상 로딩 중이 아니라는 뜻이다. */
     call("/todo", "Get", null).then((response) =>
-      this.setState({ items: response.data })
+      this.setState({ items: response.data, loading: false })
     );
   }
 
@@ -55,16 +67,48 @@ class App extends React.Component {
       </Paper>
     );
 
-    return (
-      <div className="App">
+    // navigationBar 추가
+    var navigationBar = (
+      <AppBar position="static">
+        <Toolbar>
+          <Grid justify="space-between" container>
+            <Grid item>
+              <Typography variant="h6">오늘의 할일</Typography>
+            </Grid>
+            <Grid>
+              <Button color="inherit" onClick={signout}>
+                로그아웃
+              </Button>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    );
+
+    // 로딩 중이 아닐 때 렌더링할 부분
+    var todoListPage = (
+      <div>
+        {navigationBar} {/*내비게이션 바 렌더링*/}
         <Container maxWidth="md">
           <AddTodo add={this.add} />
-          <div className="TodoList">{todoItems}
-
-          </div>
+          <div className="TodoList">{todoItems}</div>
         </Container>
       </div>
     );
+
+    // 로딩 중일 때 렌더링할 부분
+    var loadingPage = <h1> 로딩중.. </h1>;
+
+    var content = loadingPage;
+
+    if (!this.state.loading) {
+      // 로딩 중이 아니라면 todoListPage를 선택
+      content = todoListPage;
+    }
+
+    // 선택한 content 렌더링
+    return <div className="App">{content}</div>;
+
   }
 }
 
